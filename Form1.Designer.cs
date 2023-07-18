288081b768110060ae15321d5003c511e29535b1
Captain_Alekor <sashakorostelev49@gmail.com>
2023-07-18 11:55:05 +0300

    Initial commit. Full project

---
+++ Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace HardMon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripComboBox1 = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripComboBox1,
+            this.toolStripSeparator1,
+            this.toolStripButton1,
+            this.toolStripButton2});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1082, 31);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripComboBox1
+            // 
+            this.toolStripComboBox1.Items.AddRange(new object[] {
+            "CPU",
+            "GPU",
+            "RAM",
+            "Storage",
+            "Network adapter"});
+            this.toolStripComboBox1.Name = "toolStripComboBox1";
+            this.toolStripComboBox1.Size = new System.Drawing.Size(160, 31);
+            this.toolStripComboBox1.SelectedIndexChanged += new System.EventHandler(this.toolStripComboBox1_SelectedIndexChanged);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 31);
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(29, 28);
+            this.toolStripButton1.Text = "toolStripButton1";
+            this.toolStripButton1.ToolTipText = "Stop/start update";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(29, 28);
+            this.toolStripButton2.Text = "toolStripButton2";
+            this.toolStripButton2.ToolTipText = "Update";
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(488, 31);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(594, 1022);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Name";
+            this.columnHeader1.Width = 200;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Value";
+            this.columnHeader2.Width = 300;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 31);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(482, 1022);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1082, 1053);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "HardMon";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+using LibreHardwareMonitor;
+using LibreHardwareMonitor.Hardware;
+
+namespace HardMon
+{
+    public partial class Form1 : Form
+    {
+        public class UpdateVisitor : IVisitor
+        {
+            public void VisitComputer(IComputer computer)
+            {
+                computer.Traverse(this);
+            }
+            public void VisitHardware(IHardware hardware)
+            {
+                hardware.Update();
+                foreach (IHardware subHardware in hardware.SubHardware) subHardware.Accept(this);
+            }
+            public void VisitSensor(ISensor sensor) { }
+            public void VisitParameter(IParameter parameter) { }
+        }
+
+        private Computer computer;
+        private string key = string.Empty;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void GetStaticHardwareInfo(string key, ListView list)
+        {
+            list.Items.Clear();
+
+            ManagementObjectSearcher searcher = 
+                new ManagementObjectSearcher("SELECT * FROM " + key);
+
+            try
+            {
+                foreach (ManagementObject obj in searcher.Get())
+                {
+                    ListViewGroup listViewGroup;
+
+                    try
+                    {
+                        listViewGroup = list.Groups.Add(obj["Name"].ToString(),
+                                                        obj["Name"].ToString());
+                    }
+                    catch (Exception ex)
+                    {
+                        listViewGroup = list.Groups.Add(obj.ToString(), obj.ToString());
+                    }
+
+                    if (obj.Properties.Count == 0)
+                    {
+                        MessageBox.Show("Не удалось получить информацию",
+                                        "Ошибка",
+                                        MessageBoxButtons.OK,
+                                        MessageBoxIcon.Error);
+                        return;
+                    }
+
+                    foreach (PropertyData data in obj.Properties)
+                    {
+                        ListViewItem item = new ListViewItem(listViewGroup);
+
+                        if (list.Items.Count % 2 != 0)
+                            item.BackColor = Color.White;
+                        else item.BackColor = Color.WhiteSmoke;
+
+                        item.Text = data.Name;
+
+                        if (data.Value != null && !string.IsNullOrEmpty(data.Value.ToString()))
+                        {
+                            switch (data.Value.GetType().ToString()) {
+                                case "System.String[]":
+                                    string[] stringData = data.Value as string[];
+                                    string resStr = string.Empty;
+                                    foreach (string s in stringData)
+                                        resStr += $"{s} ";
+                                    item.SubItems.Add(resStr);
+                                    break;
+                                case "System.UInt16[]":
+                                    ushort[] ushortData = data.Value as ushort[];
+                                    string resStr2 = string.Empty;
+                                    foreach (ushort u in ushortData)
+                                        resStr2 += $"{Convert.ToString(u)} ";
+                                    item.SubItems.Add(resStr2);
+                                    break;
+                                default:
+                                    item.SubItems.Add(data.Value.ToString());
+                                    break;
+                            }
+                            list.Items.Add(item);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message,
+                                "Ошибка",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Error);
+            }
+        }
+
+        private void GetDynamicHardwareInfo(string key)
+        {
+            List<HardwareType> hwType = new List<HardwareType>();
+            string info = string.Empty;
+            switch (key)
+            {
+                case "Win32_Processor":
+                    hwType.Add(HardwareType.Cpu);
+                    break;
+                case "Win32_VideoController":
+                    hwType.Add(HardwareType.GpuIntel);
+                    hwType.Add(HardwareType.GpuNvidia);
+                    hwType.Add(HardwareType.GpuAmd);
+                    break;
+                case "Win32_PhysicalMemory":
+                    hwType.Add(HardwareType.Memory);
+                break;
+                case "Win32_DiskDrive":
+                    hwType.Add(HardwareType.Storage);
+                    break;
+                case "Win32_NetworkAdapter":
+                    hwType.Add(HardwareType.Network);
+                    break;
+                default:
+                    return;
+            }
+            foreach (HardwareType hwtype in hwType) {
+                foreach (IHardware hardware in computer.Hardware)
+                {
+                    if (hardware.HardwareType != hwtype)
+                        continue;
+                    info += "\t" + hardware.Name + "\n";
+                    foreach (ISensor sensor in hardware.Sensors)
+                    {
+                        info += $"{sensor.Name}    {sensor.SensorType}: {sensor.Value}\n";
+                    }
+                    info += "\n";
+                }
+            }
+            richTextBox1.Text = info;
+        }
+
+        private void toolStripComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            key = string.Empty;
+            switch (toolStripComboBox1.SelectedItem.ToString())
+            {
+                case "CPU":
+                    key = "Win32_Processor";
+                    break;
+                case "GPU":
+                    key = "Win32_VideoController";
+                    break;
+                case "Battery":
+                    key = "Win32_Battery";
+                    break;
+                case "RAM":
+                    key = "Win32_PhysicalMemory";
+                    break;
+                case "Storage":
+                    key = "Win32_DiskDrive";
+                    break;
+                case "Cache":
+                    key = "Win32_CacheMemory";
+                    break;
+                case "Network adapter":
+                    key = "Win32_NetworkAdapter";
+                    break;
+                default:
+                    return;
+            }
+            GetDynamicHardwareInfo(key);
+            GetStaticHardwareInfo(key, listView1);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //toolStripComboBox1.SelectedIndex = 0;
+            computer = new Computer
+            {
+                IsCpuEnabled = true,
+                IsGpuEnabled = true,
+                IsMemoryEnabled = true,
+                IsMotherboardEnabled = true,
+                IsControllerEnabled = true,
+                IsNetworkEnabled = true,
+                IsStorageEnabled = true
+            };
+            computer.Open();
+            backgroundWorker1.RunWorkerAsync();
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            GetDynamicHardwareInfo(key);
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (true)
+            {
+                computer.Accept(new UpdateVisitor());
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            computer.Close();
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+            {
+                timer1.Stop();
+                toolStripButton1.Checked = true;
+            }
+            else
+            {
+                timer1.Start();
+                toolStripButton1.Checked = false;
+            }
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            if (key == string.Empty) return;
+            GetStaticHardwareInfo(key, listView1);
+        }
+    }
+}
